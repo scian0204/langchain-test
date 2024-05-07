95138bfa36b058c8e7eef8e32daea2284ad31534
scian0204 <scian0204@gmail.com>
2024-05-07 09:48:37 +0900

    viewport 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'chat ui',
   description: 'chat with llama',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1.0,
-    maximumScale: 1.0,
-    userScalable: false,
-  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
 };
 
 export default function RootLayout({
